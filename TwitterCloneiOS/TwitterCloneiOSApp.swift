8b5efe6b3beba7cb5bb25eea8fcacf2ba4330697
Darserg <Darserg1212>
2024-07-10 12:45:47 +0400

    Initial Commit

---
+++ TwitterCloneiOS/TwitterCloneiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneiOSApp.swift
+//  TwitterCloneiOS
+//
+//  Created by Сергей Дарьин on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
